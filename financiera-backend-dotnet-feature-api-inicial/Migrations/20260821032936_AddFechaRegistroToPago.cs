bc691f06f0160e69cc5e9832b189029349f31b54
MoneyPineA <moneypine2026@outlook.com>
2026-08-20 21:32:59 -0600

    Agregado a la tabla Recibo

---
+++ financiera-backend-dotnet-feature-api-inicial/Migrations/20260821032936_AddFechaRegistroToPago.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiEjemplo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFechaRegistroToPago : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "fecha_registro",
+                table: "pago",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "fecha_registro",
+                table: "pago");
+        }
+    }
+}
